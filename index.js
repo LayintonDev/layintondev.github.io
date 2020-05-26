f88ae00de02b877ecf09d853c9e80937e2082c35
LayintonDev <65630704+LayintonDev@users.noreply.github.com>
2020-05-26 16:57:23 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+    document.querySelector("title").innerHTML ="democatalogue";
+    var x = document.getElementById("demoCataloque");
+    x.style.color="white";
+   
+    function search() {
+      var layinton = document.querySelector(".navbar-brand");
+      layinton.style.color = "teal";
+      layinton.style.fontSize = "35px";
+    }
+ 
